6dffe74ccefaeb26cc8b3272261558e61e897bcc
Steevan BARBOYON <steevan.barboyon@gmail.com>
2025-05-19 20:06:17 +0200

    Install Symfony

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Steevanb\DevOps;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
