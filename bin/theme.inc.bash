685de686efe8a20a7653aa7179b278f50fd7d465
Steevan BARBOYON <steevan.barboyon@gmail.com>
2025-05-19 19:36:09 +0200

    Create Docker stack and binaries

---
+++ bin/theme.inc.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function theme_block() {
+    local titleLength="${#2}"
+    echo -en "\n\e[${1}m\e[1;37m    "
+    for x in $(seq 1 ${titleLength}); do echo -n " "; done;
+    echo -e "\e[0m"
+
+    echo -e "\e[${1}m\e[1;37m  ${2}  \e[0m"
+    echo -en "\e[${1}m\e[1;37m    "
+    for x in $(seq 1 ${titleLength}); do echo -n " "; done;
+    echo -e "\e[0m\n"
+}
+
+function theme_title() {
+    if [ ${VERBOSITY_LEVEL} == 0 ]; then
+        echo -n "${1}..."
+    else
+        theme_block 46 "${1}"
+    fi
+
+    theme_last_title_time=$(date +%s%3N)
+}
+
+
+function theme_ok() {
+    elapsed_milliseconds=$(( $(date +%s%3N) - theme_last_title_time))
+
+    if [ ${VERBOSITY_LEVEL} == 0 ]; then
+        echo -e " [ \e[0;32mOK\e[0m ] ${elapsed_milliseconds}ms"
+    else
+        theme_block 42 "Ok - ${elapsed_milliseconds}ms"
+    fi
+}
