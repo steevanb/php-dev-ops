1a52614837ca4eb9c769692b8b29292e7882f97a
Steevan BARBOYON <steevan.barboyon@gmail.com>
2025-05-21 20:12:23 +0200

    Move CI docker image to github

--- bin/docker/dockerise.php.inc.bash
+++ bin/docker/dockerise.php.inc.bash
@@ -4,4 +4,4 @@ set -eu
 
 source "${PROJECT_PATH}"/config/docker/docker-image-names.inc.bash
 
-DOCKERISE_IMAGE_NAME="${DOCKER_PHP_IMAGE_NAME}" source "${PROJECT_PATH}"/bin/docker/dockerise.inc.bash
+DOCKERISE_IMAGE_NAME="${DOCKER_DEV_IMAGE_NAME}" source "${PROJECT_PATH}"/bin/docker/dockerise.inc.bash
